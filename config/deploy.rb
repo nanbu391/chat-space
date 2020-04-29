1c688406eaabe6c5fcb649eba0fe2731d3dba39b
nanbu391 <nabe48665@gmail.com>
2020-04-29 20:02:46 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.13.0'
 
 # Capistranoのログの表示に利用する
-set :application, '<chat-space>'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:nanbu391/chat-space.git'
